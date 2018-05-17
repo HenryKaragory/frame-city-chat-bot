d8eed4035463932366ed8271f4ef06d7475a1c03
Henry Karagory <henrytkaragory@gmail.com>
2018-05-17 09:04:11 -0400

    Initial commit

---
+++ chatbot/bot.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+import json
+import requests
+
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = ''
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def handle_verification():
+  verify_token = ''
+
+  # Parse query params
+  mode = request.args['hub.mode']
+  token = request.args['hub.verify_token']
+  challenge = request.args['hub.challenge']
+
+  if mode and token:
+    if mode == 'subscribe' and token == verify_token:
+      print('WEBHOOK VERIFIED...')
+      return 'ok'
+    else:
+      print('WEBHOOK NOT VERIFIED...')
+      return 'not verified', 403
+
+@app.route('/', methods=['POST'])
+def handle_messages():
+  body = request.get_json()
+  print(type(body))
+  print(body)
+
+  if body['object'] == 'page':
+    for entry in body['entry']:
+      webhook_event = entry['messaging'][0]['message']
+      print(webhook_event)
+    return 'EVENT RECEIVED...'
+  else:
+    return 'bad', 404
+
+def respond():
+  params = {'access_token': PAT}
+  headers = {'Content-type': 'application/json'}
+  body = {
+    'messaging_type': 'RESPONSE',
+    'recipient': {'id': recipient}, 
+    'message': {'text': 'Hello!'}
+    }
+  r = requests.post('https://graph.facebook.com/v2.6/me/messages')
+
+
+if __name__ == '__main__':
+  app.run()
