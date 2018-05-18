f991f511ef9d5146f8510e23800cf29dd20800da
Henry Karagory <henrytkaragory@gmail.com>
2018-05-17 22:12:37 -0400

    Refactored code, moved witai and facebook send api calls to helper modules

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -3,13 +3,16 @@ import json
 import requests
 import os
 
-PAT = os.environ['PAT']
+from . import witai_helpers
+from . import send_helpers
+
+fb_verify_token = os.environ['VERIFYTOKEN']
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  verify_token = os.environ['VERIFYTOKEN']
+  """Implements webhook verification as outlined by the Messenger Platform"""
 
   # Parse query params
   mode = request.args['hub.mode']
@@ -17,7 +20,7 @@ def handle_verification():
   challenge = request.args['hub.challenge']
 
   if mode and token:
-    if mode == 'subscribe' and token == verify_token:
+    if mode == 'subscribe' and token == fb_verify_token:
       print('WEBHOOK VERIFIED...')
       return challenge
     else:
@@ -25,34 +28,25 @@ def handle_verification():
       return 'not verified', 403
 
 @app.route('/', methods=['POST'])
-def handle_messages():
-  body = request.get_json()
+def handle_webhook_events():
+  """
+  Primary handler for webhook events. Determines the message intent
+  and responds accordingly.
+  """
 
+  body = request.get_json()
   if body['object'] == 'page':
-    print(body)
     for entry in body['entry']:
-      print(entry)
-      webhook_event = entry['messaging'][0]['message']
-      sender_id = entry['messaging'][0]['sender']['id']
-      respond(sender_id)
+    
+      if 'messaging' in entry:
+        message_text = entry['messaging'][0]['message']['text']
+        sender_id = entry['messaging'][0]['sender']['id']
+        response = witai_helpers.determine_response(message_text)
+        send_helpers.respond_text(sender_id, response)
+    
     return 'EVENT RECEIVED...'
   else:
     return 'bad', 404
-
-def respond(recipient_id):
-  params = {'access_token': PAT}
-  headers = {'Content-type': 'application/json'}
-  body = {
-    'messaging_type': 'RESPONSE',
-    'recipient': {'id': recipient_id}, 
-    'message': {'text': 'This is the app!'}
-    }
-  r = requests.post('https://graph.facebook.com/v2.6/me/messages',
-      headers=headers,
-      params=params,
-      json=body
-    )
-
 
 if __name__ == '__main__':
   app.run()
