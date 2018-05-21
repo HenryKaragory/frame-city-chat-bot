936f8705f8133bfa7d0aa44fd63f2c2afa2bf90c
Henry Karagory <henrytkaragory@gmail.com>
2018-05-20 20:41:32 -0400

    Initialized testing folder

---
+++ test/db_helpers_test.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+
+sys.path.append('..')
+from chatbot import db_helpers
+
+class TestDBHelpers(unittest.TestCase):
+
+	def setUp(self):
+		# TODO CREATE THE CONNECTION OBJECT
+
+	def testDown(self):
+		# TODO CLOSE THE CONNECTION OBJECT
+
+
+if __name__ == "__main__":
+	unittest.main()
